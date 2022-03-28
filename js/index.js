8849aca4d4a6428875d297ee7c122a82f96c4a98
SoriaJulia <soriajulias@gmail.com>
2022-03-28 16:39:42 -0300

    add particles.js + gradients

---
+++ js/index.js
@@ -0,0 +1,15 @@
+window.onload= function() {
+  Particles.init
+({
+    selector:'.background',
+    color:['a42b50', '17464a','297a82', '0b0934'],
+    connectParticles:true,
+        maxParticles:200,
+    responsive:[{
+      breakpoint:850,
+      options:{
+        maxParticles:50,
+      }
+    }]
+  });
+};
